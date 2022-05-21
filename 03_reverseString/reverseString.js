19f6a60e582e6a0c085e3db2a14ca94fe2c8514d
jpoe <16984557+jpoehl@users.noreply.github.com>
2022-05-21 11:45:10 +0200

    Write function to reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringArray = string.split("");
+    const reversedArray = [];
 
+    for (let l = stringArray.length; l >= 0; l--) {
+        reversedArray[stringArray.length - l] = stringArray[l];
+    }
+
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
