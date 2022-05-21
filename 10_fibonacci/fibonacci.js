3cadff907981727d827be0dd871c5b9aa0642e2a
jpoe <16984557+jpoehl@users.noreply.github.com>
2022-05-21 18:46:35 +0200

    Write functions using Array.push

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    const sequence = [1, 1];
+    const nth = +(index);
 
+    if (nth < 0) {
+        return "OOPS";
+    } else {
+
+        for (let i = 2; i < nth; i++) {
+            sequence.push(sequence[i - 1] + sequence[i - 2]);
+        }
+
+        return sequence[nth - 1];
+    }
 };
 
 // Do not edit below this line
