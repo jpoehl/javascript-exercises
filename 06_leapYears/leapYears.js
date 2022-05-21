39328677ff05cd7f12deb6bc4fd12dd7afc6fde4
jpoe <16984557+jpoehl@users.noreply.github.com>
2022-05-21 13:01:41 +0200

    Write function using if/else if statement

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    if (Number.isInteger(year / 400)) {
+        return true;
+    } else if(Number.isInteger(year / 100)) {
+        return false;
+    } else if(Number.isInteger(year / 4)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
