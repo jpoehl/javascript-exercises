7cad10831808e3626bab544bd720f3f68c8caf63
jpoe <16984557+jpoehl@users.noreply.github.com>
2022-05-21 19:38:21 +0200

    Write function using Date methods

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (persons) {
+    const ages = [];
+    const year = new Date().getFullYear();
 
+    for (person of persons) {
+        if (person.yearOfDeath === undefined) {
+            ages.push(year - person.yearOfBirth);
+        } else {
+            ages.push(person.yearOfDeath - person.yearOfBirth);
+        }
+    }
+
+    const maxAge = Math.max(...ages);
+    const index = ages.indexOf(maxAge);
+
+    return persons[index];
 };
 
 // Do not edit below this line
