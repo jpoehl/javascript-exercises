f09968cd123dd1eca66945f7b50166ff0ea4a2a2
jpoe <16984557+jpoehl@users.noreply.github.com>
2022-05-21 18:28:04 +0200

    Write function using regex and Array.reverse

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const wordLower = word.toLowerCase();
+    const wordCleaned = wordLower.replace(/[^a-z]/g, '');
+    const wordArray = wordCleaned.split('');
+    const inverted = wordArray.reverse().join('');
 
+    return wordCleaned === inverted;
 };
 
 // Do not edit below this line
