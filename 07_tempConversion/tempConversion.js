929a1071f2bd47c3ba1fc874d34dfa0c094ef367
jpoe <16984557+jpoehl@users.noreply.github.com>
2022-05-21 17:22:23 +0200

    Write functions using rounding with Number.EPSILON

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp_fahrenheit) {
+  const temp_celsius = (temp_fahrenheit - 32) * 5 / 9;
+  return Math.round((temp_celsius + Number.EPSILON) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp_celsius) {
+  const temp_fahrenheit =  temp_celsius * 9 / 5 + 32;
+  return Math.round((temp_fahrenheit + Number.EPSILON) * 10) / 10;
 };
 
 // Do not edit below this line
