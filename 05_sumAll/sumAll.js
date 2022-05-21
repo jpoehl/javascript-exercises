46e9fb99c651553821b92ecd83bc03627e7bbe89
jpoe <16984557+jpoehl@users.noreply.github.com>
2022-05-21 12:25:31 +0200

    Write function using min/max and a for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (typeof (num1) != "number" || typeof (num2) != "number" || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else {
+        const maxNum = Math.max(num1, num2);
+        const minNum = Math.min(num1, num2);
+        let sum = 0;
+
+        for (let i = minNum; i <= maxNum; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
 
 };
 
